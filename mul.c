a31a75bd5023dca2d21ab303b1f9509e2a4e66ed
saviour623 <saviourme33@gmail.com>
2023-12-30 16:47:36 +0100

    updated

--- mul.c
+++ mul.c
@@ -5,7 +5,9 @@
 #define stopTime(n) ((n) = clock() - (n))
 #define printTime(n) printf("%f\n", (n) / (double) CLOCKS_PER_SEC)
 #define reverseBf generic_reversebuff
-
+#ifdef __GNUC__
+     #define restrict __restrict__
+#endif
 const int extPow10[] = {
 	10, /* 1 */
 	100, /* 2 */
@@ -20,21 +22,25 @@ const int extPow10[] = {
 #define LIMB_SZ 100000
 #define MULSZ_MAX 6
 
-int *BigAdd(int lmb1[], size_t szLmb1, int lmb2[], size_t szLmb2)
+__attribute__((nonnull)) int BigMulAdd(int * __restrict__ resAdd, int lmb1[], size_t szLmb1, int lmb2[], size_t szLmb2)
 {
-	int *resAdd = malloc(sizeof (int) * (szLmb1 + szLmb2));
-	register size_t oo = 0, tSz = szLmb1 + szLmb2;
-	register unsigned int adLmb = 0, crLmb = 0;
+	register unsigned int oo = 0, adLmb = 0, crLmb = 0;
 
-	resAdd[0] = lmb1[--szLmb];
-
-	for (; oo < (szlmb2 - 1); oo++)
+	resAdd[oo++] = lmb1[--szLmb1];
+	for (; szLmb1; oo++)
 	{
-		adLmb = lmb2[szLmb2 - 1] + lmb2[szLmb1] + crLmb;
+		adLmb = lmb2[--szLmb2] + lmb1[--szLmb1] + crLmb;
+		crLmb = 0;
 		if (adLmb > 0x1869f)
 		{
+			resAdd[oo] = adLmb % LIMB_SZ;
+			crLmb = adLmb / LIMB_SZ;
+			continue;
 		}
+		resAdd[oo] = adLmb;
 	}
+	resAdd[oo] = lmb2[0] + crLmb;
+	return oo + 1;
 }
 int *BigMul(int lmb1[], int lmb2[])
 {
@@ -48,31 +54,32 @@ int *BigMul(int lmb1[], int lmb2[])
 	/*130042,55780,53342,40281*/
 	/* 00000000000000011000011010011111 */
 }
-int toLimb(int lmbBf[], size_t size, long long int val)
+__attribute__((nonnull)) int toLimb(int lmbBf[], size_t size, long long int val)
 {
-	register int oo;
-	if (size == 0 || lmbBf == NULL)
-		return -1;
+	register int oo, tmp;
 	for (oo = 0; oo < size && val; oo++, (val /= LIMB_SZ))
 		lmbBf[oo] = val % LIMB_SZ;
-	size = 0;
-	reverseBf(lmbBf, size, oo);
-	return (oo << 1 | 1);
+	tmp = oo; size = 0;
+	reverseBf(lmbBf, size, tmp);
+	return oo;
 }
 int main(void)
 {
 	long long int c;
 	clock_t n, p;
 	int bf[6], l = 0;
-
-	startTime(n);
-
+	int a[] = {472481, 55620, 40281};
+	int b[] = {130037, 83298, 97722};
+	int *resAdd = malloc(sizeof (int) * (6));
 	l = toLimb(bf, 6, 6209427609113893LL);
-
+	startTime(n);
+	c = BigMulAdd(resAdd, a, 3, b, 3);
 	stopTime(n);
 	printTime(n);
 
-	for (int i = 0; i < l; i++)
-		printf("%d\n", bf[i]);
+	//for (int i = 0; i < l; i++)
+	//printf("%d\n", bf[i]);
+	for (int i = 0; i < c; i++)
+		printf("%d\n", resAdd[i]);
 	putchar(10);
 }
